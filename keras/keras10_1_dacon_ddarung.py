3d693b796497b6cc7124fa2d1ff17718b9a482d8
home <amblcw@naver.com>
2024-01-07 22:27:17 +0900

    .

--- keras/keras10_1_dacon_ddarung.py
+++ keras/keras10_1_dacon_ddarung.py
@@ -27,8 +27,10 @@ submission_csv = pd.read_csv(path+"submission.csv")
 
 #### 결측치 처리 1. 제거 #### 나중에 드랍 말고 다른 것도 적용해보기
 # print(train_csv.isnull().sum()) 아래와 같음
-print(train_csv.isna().sum())
-print(test_csv.isna().sum())
+# print(train_csv.isna().sum())
+# print(test_csv.isna().sum())
+
+
 # train_csv = train_csv.dropna()
 
 nan_index = np.where(np.isnan(train_csv))
@@ -52,11 +54,15 @@ for i in range(len(row2)):
     next = test_csv.iloc[row2[i]+1,colum2[i]]
     test_csv.iloc[row2[i],colum2[i]] = (pre + next)/2
 
-print(train_csv.isna().sum(), test_csv.isna().sum())
+# print(train_csv.isna().sum(), test_csv.isna().sum())
 
 
 train_csv = train_csv.fillna(train_csv.mean())
 test_csv = test_csv.fillna(test_csv.mean())
+# train_csv = train_csv.fillna(0)
+# test_csv = test_csv.fillna(0)
+
+
 
 # print(train_csv.isna().sum(), test_csv.isna().sum())
 
@@ -68,55 +74,55 @@ r2_list = []
 submit_list = []
 
 r2=0
-for i in range(10):
+# for i in range(10):
 # while r2<0.69:
-    r = int(np.random.uniform(1,1000))
-    r = 97
-    x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.9,random_state=r)
-    print(f"{x_train.shape=} , {y_train.shape=}")   #x_train.shape=(1167, 9) , y_train.shape=(1167,)
+r = int(np.random.uniform(1,1000))
+r = 285
+x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.9,random_state=r)
+print(f"{x_train.shape=} , {y_train.shape=}")   #x_train.shape=(1167, 9) , y_train.shape=(1167,)
 
-    # model
-    model = Sequential()
-    # model.add(Dense(1,input_dim=9))
-    model.add(Dense(16,input_dim=9))
-    # model.add(Dense(32))
-    # model.add(Dense(48))
-    # model.add(Dense(64))
-    # model.add(Dense(96))
-    # model.add(Dense(128))
-    # model.add(Dense(198))
-    # model.add(Dense(256))
-    # model.add(Dense(198))
-    # model.add(Dense(128))
-    # model.add(Dense(96))
-    # model.add(Dense(64))
-    # model.add(Dense(48))
-    # model.add(Dense(32))
-    # model.add(Dense(16))
-    model.add(Dense(8))
-    model.add(Dense(4))
-    model.add(Dense(2))
-    model.add(Dense(1))
+# model
+model = Sequential()
+# model.add(Dense(1,input_dim=9))
+model.add(Dense(16,input_dim=9))
+# model.add(Dense(32))
+# model.add(Dense(48))
+# model.add(Dense(64))
+# model.add(Dense(96))
+# model.add(Dense(128))
+# model.add(Dense(198))
+# model.add(Dense(256))
+# model.add(Dense(198))
+# model.add(Dense(128))
+# model.add(Dense(96))
+# model.add(Dense(64))
+# model.add(Dense(48))
+# model.add(Dense(32))
+# model.add(Dense(16))
+model.add(Dense(8))
+model.add(Dense(4))
+model.add(Dense(2))
+model.add(Dense(1))
 
-    #compile & fit
-    model.compile(loss='mse',optimizer='adam')
-    model.fit(x_train,y_train,epochs=1024,batch_size=16,verbose=2)
+#compile & fit
+model.compile(loss='mse',optimizer='adam')
+model.fit(x_train,y_train,epochs=1024,batch_size=4,verbose=2)
 
-    #evaluate & predict
-    loss = model.evaluate(x_test,y_test)
-    y_predict = model.predict(x_test)
-    r2 = r2_score(y_test,y_predict)
-    y_submit = model.predict(test_csv)
+#evaluate & predict
+loss = model.evaluate(x_test,y_test)
+y_predict = model.predict(x_test)
+r2 = r2_score(y_test,y_predict)
+y_submit = model.predict(test_csv)
 
-    print(f"{r=}\nRMSE: {math.sqrt(loss)}\n{r2=}")
-    time.sleep(1.5)
-        
+print(f"{r=}\nRMSE: {math.sqrt(loss)}\n{r2=}")
+time.sleep(1.5)
 
-    # print(y_submit)
-    # print(y_submit.shape)   #(751, 1)
-    loss_list.append(loss)
-    r2_list.append(r2)
-    submit_list.append(y_submit)
+
+# print(y_submit)
+# print(y_submit.shape)   #(751, 1)
+loss_list.append(loss)
+r2_list.append(r2)
+submit_list.append(y_submit)
 
 print(loss_list,r2_list,sep='\n')
 print(f"LOSS mean: {np.mean(loss_list)}\nR2 mean: {np.mean(r2_list)}")
@@ -161,3 +167,121 @@ submission_csv.to_csv(path+f"submission_{dt.day}day{dt.hour}-{dt.minute}.csv",in
 # [0.6818519664927092, 0.6777695788563591, 0.6841834822383354, 0.6742688809328556, 0.6834353214622559, 0.6808850086508238, 0.6803072467981431, 0.680834011104618, 0.6820872999065146, 0.6808045040107468] 
 # LOSS mean: 2075.6867431640626
 # R2 mean: 0.6806427300453362
+
+#8421레이어 2048회 훈련
+# [2174.57763671875, 2067.84814453125, 2111.7958984375, 2044.9608154296875, 2075.06640625, 2110.235107421875, 2073.840087890625, 2088.180419921875, 2168.029052734375, 2075.403564453125]
+# [0.6654277835203164, 0.681848736524346, 0.6750870976179174, 0.6853701257182265, 0.680738197932573, 0.6753272908454534, 0.6809268610012049, 0.6787204867731416, 0.6664353027665137, 0.6806863051852762]  
+# LOSS mean: 2098.9937133789062
+# R2 mean: 0.6770568187884969
+
+#88421레이어 512회 훈련, batch_size=4
+# RMSE: 45.34807601262566
+# r2=0.6836027245280307
+# [2056.447998046875]
+# [0.6836027245280307]
+# LOSS mean: 2056.447998046875
+
+#동일 조건, Nan 처리를 drop으로 했을 때
+# [3606.00732421875]
+# [0.5180706974347669]
+# LOSS mean: 3606.00732421875
+# R2 mean: 0.5180706974347669
+
+# train 비중을 0.7로 낮췄을 때
+# RMSE: 53.0158437403386
+# r2=0.5774311018227056
+# [2810.6796875]
+# [0.5774311018227056]
+# LOSS mean: 2810.6796875
+# R2 mean: 0.5774311018227056
+
+#fillna(0)
+# RMSE: 45.51148657378981
+# r2=0.6813183622943629
+# [2071.29541015625]
+# [0.6813183622943629]
+# LOSS mean: 2071.29541015625
+# R2 mean: 0.6813183622943629
+
+#fillna(0) 2회차
+# RMSE: 49.15514491367485
+# r2=0.6282482922552581
+# [2416.228271484375]
+# [0.6282482922552581]
+# LOSS mean: 2416.228271484375
+# R2 mean: 0.6282482922552581
+
+#훈련 횟수 128로 줄여보기
+# RMSE: 70.34018552164012
+# r2=0.2387593048768234
+# [4947.74169921875]
+# [0.2387593048768234]
+# LOSS mean: 4947.74169921875
+# R2 mean: 0.2387593048768234
+
+# Traceback (most recent call last):
+#   File "<frozen runpy>", line 198, in _run_module_as_main
+#   File "<frozen runpy>", line 88, in _run_code    
+#   File "c:\Users\wlily\.vscode\extensions\ms-python.python-2023.22.1\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher\__main__.py", line 
+# 91, in <module>
+#     main()
+#   File "c:\Users\wlily\.vscode\extensions\ms-python.python-2023.22.1\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher\__main__.py", line 
+# 47, in main
+#     launcher.connect(host, port)
+#   File "c:\Users\wlily\.vscode\extensions\ms-python.python-2023.22.1\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher/../..\debugpy\launcher\__init__.py", line 27, in connect
+#     sock.connect((host, port))
+# OSError: [WinError 10049] 요청한 주소는 해당 컨텍 
+# 스트에서 유효하지 않습니다
+
+#86421레이어
+# RMSE: 45.35227510745051
+# r2=0.683544161425371
+# [2056.828857421875]
+# [0.683544161425371]
+# LOSS mean: 2056.828857421875
+# R2 mean: 0.683544161425371
+
+#위와 동일, 배치사이즈 32로
+# [2098.32568359375, 2125.633544921875, 2091.154541015625, 2084.49072265625, 2099.593994140625, 2080.016357421875, 2112.98291015625, 2204.400634765625, 2064.084228515625, 2096.63232421875]
+# [0.677159593163063, 0.6729581135554364, 0.678262938757972, 0.6792881791896368, 0.6769644741418188, 0.6799766012497923, 0.6749045161725038, 0.6608393051206192, 0.6824278428978767, 0.6774201465101761]
+# LOSS mean: 2105.731494140625
+# R2 mean: 0.6760201710758895
+
+#어떻게 해도 처음이 제일 좋으니 처음 조건 그대로 다시 다만 R2 값은 오히려 낮았기에 R2값으로 판단 불가, 전부 올려서 테스트해보기
+# RMSE: 54.526629460406774
+# r2=0.6203538588450952
+# [2973.1533203125]
+# [0.6203538588450952]
+
+# RMSE: 54.542009593953125
+# r2=0.620139696021671
+# [2974.830810546875]
+# [0.620139696021671]
+
+# RMSE: 54.02795364800174
+# r2=0.6272662178842985
+# [2919.019775390625]
+# [0.6272662178842985]
+
+# RMSE: 53.92531592179711
+# r2=0.6286810640834528
+# [2907.939697265625]
+# [0.6286810640834528]
+
+#batch_size를 오히려 16으로 늘렸을 때
+# RMSE: 55.36393045044648
+# r2=0.6086047510512438
+# [3065.164794921875]
+# [0.6086047510512438]
+
+# 8421레이어
+#RMSE: 54.061398110158045
+# r2=0.6268046350786065
+# [2922.634765625]
+# [0.6268046350786065]
+
+#다시 초기 레이어
+# RMSE: 55.55196662669783
+# r2=0.6059415976425613
+# [3086.02099609375]
+# [0.6059415976425613]
--- python/function_class_diff.py
+++ python/function_class_diff.py
@@ -1,5 +1,5 @@
 #함수 
-'''
+''' 문법
 def 함수이름(인자):
     구현 내용
 '''
@@ -8,6 +8,7 @@ def counter1(n):
     return n
 
 print("counter1")
+
 a = 10
 while a>0:
     a = counter1(a)
@@ -15,20 +16,20 @@ while a>0:
     
 
 #클래스
-'''
+''' 문법
 class 클래스이름:
     맴버변수와 맴버함수들
 '''
 class Counter:
-    __n = 0
-    def __init__(self,n):
+    __n = 0                 #맴버 변수, __를 붙이면 외부에서 접근불가
+    def __init__(self,n):   #생성자
         self.__n = n
         
     def counter2(self):
         self.__n -= 1
         return self.__n
     
-    def get_n(self):
+    def get_n(self):        #외부에서 접근불가능하기에 접근용 함수
         # print(self.__n)
         return self.__n
     
@@ -36,6 +37,7 @@ c = Counter(10)
 other_c = Counter(5)
 
 print("counter2")
+
 while c.get_n() > 0 or other_c.get_n() > 0:
     if c.get_n() > 0:
         print(c.counter2() , end=' ')
@@ -53,8 +55,8 @@ while c.get_n() > 0 or other_c.get_n() > 0:
 
 반면에 클래스는 함수에 더해서 변수를 묶어서 재사용을 가능하게 해줍니다
 또한 이 변수는 클래스 내부에 존재하기에 외부로 부터 보호받을 수도 있습니다 
-더불어서 클래스는 사용할 때 인스턴스를 생성하여 사용하기에 여러개의 카운터를 동시에 진행할 수도 있습니다 
-만일 함수로 동시에 여러개의 카운터를 진행하려면 그만큼 다른 이름의 외부 변수를 만들어 줘야하며 이는 재사용성을 떨어뜨립니다 
+더불어서 클래스는 사용할 때 인스턴스를 생성하여 사용하기에 여러개의 counter를 동시에 진행할 수도 있습니다 
+만일 함수로 동시에 여러개의 counter를 진행하려면 그만큼 다른 이름의 외부 변수를 만들어 줘야하며 이는 재사용성을 떨어뜨립니다 
 
 따라서 오직 입력값에만 영향을 받는 순수함수 같은 경우엔 함수로 작성하는 것이 유리하지만
 외부 변수를 이용해야한다던가 상태를 저장해야하는 경우에는 변수를 같이 묶을 수 있는 클래스가 재사용에 유리하다 생각합니다
