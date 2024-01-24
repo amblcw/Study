b3a160b4054c57ed866beb67c14b62229b245214
amblcw <amblcw@naver.com>
2024-01-24 12:18:21 +0900

    catdog

--- keras/keras37_ImageDataGenerator3_CatDog.py
+++ keras/keras37_ImageDataGenerator3_CatDog.py
@@ -5,6 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, load_model
 from keras.layers import Conv2D, Dense, Flatten, Dropout, MaxPooling2D, BatchNormalization
 from keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
 import numpy as np
 import time
 
@@ -17,26 +18,25 @@ BATCH_SIZE = int(100)
 
 train_data_gen = ImageDataGenerator(
     rescale=1./255,
-    horizontal_flip=True,
-    vertical_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    rotation_range=5,
-    zoom_range=1.2,
-    shear_range=0.7,
-    fill_mode='nearest'
+    # horizontal_flip=True,
+    # vertical_flip=True,
+    # width_shift_range=0.1,
+    # height_shift_range=0.1,
+    # rotation_range=5,
+    # zoom_range=1.2,
+    # shear_range=0.7,
+    # fill_mode='nearest'
 )
 
 xy_train_data = train_data_gen.flow_from_directory(
     train_path,
-    target_size=(200,200),
+    target_size=(140,140),
     batch_size=BATCH_SIZE,        #batch_size 너무 크게주면 에러나옴
     class_mode='binary',
     shuffle=False
 )
 
-end_time = time.time()
-print(f"time: {end_time-start_time:.4f}sec")
+
 
 # x = np.array(np.arange(0,27)).reshape(3,3,3,1)
 # y = np.array(np.arange(0,27)).reshape(3,3,3,1)
@@ -71,6 +71,54 @@ for i in range(int(20000 / BATCH_SIZE)):
 print(failed_i) # [70, 115]
 
 print(f"{x.shape=}\n{y.shape=}")    # x.shape=(1000, 200, 200, 3) y.shape=(1000,)
-
 # 파일탐색기 - 사진크기순 분류 - 맨 밑으로 내리면 지정되지 않음 존재
 # 불량파일: cat/666.jpg cat/10404.jpg dog/11702.jpg
+
+end_time = time.time()
+print(f"time: {end_time-start_time:.4f}sec")  # time: 284.4365sec
+
+r = int(np.random.uniform(1,1000))
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=r, stratify=y)
+
+# model
+model = Sequential()
+model.add(Conv2D(32,(2,2),padding='valid',strides=2,input_shape=x_train.shape[1:]))
+model.add(MaxPooling2D())
+model.add(Conv2D(32,(2,2),padding='valid',strides=2))
+model.add(BatchNormalization())
+model.add(MaxPooling2D())
+model.add(Dropout(0.15))
+model.add(Conv2D(32,(2,2),padding='same',activation='relu'))
+model.add(Conv2D(32,(2,2),padding='same',activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D())
+model.add(Dropout(0.15))
+model.add(Conv2D(64,(2,2),padding='same',activation='relu'))
+model.add(Conv2D(64,(2,2),padding='same',activation='relu'))
+model.add(BatchNormalization())
+# model.add(MaxPooling2D())
+# model.add(Dropout(0.15))
+model.add(Flatten())
+model.add(Dense(1024,activation='relu'))
+model.add(Dense(128,activation='relu'))
+model.add(Dense(1,activation='sigmoid'))
+
+# compile & fit
+s_time = time.time()
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['acc'])
+es = EarlyStopping(monitor='val_loss',mode='auto',patience=50,restore_best_weights=True)
+hist = model.fit(x_train,y_train,epochs=1024,batch_size=64,validation_data=(x_test,y_test),verbose=2,callbacks=[es])
+e_time = time.time()
+
+# evaluate & predict
+loss = model.evaluate(x_test,y_test,verbose=0)
+
+print(f"loading time: {end_time-start_time:.4f}sec")  # time: 284.4365sec
+print(f"fitting time: {e_time-s_time:.4f}sec")
+print(f"{r=}\nLOSS: {loss[0]:.6f}\nACC:  {loss[1]:.6f}")
+model.save(path+f"model_save\\acc_{loss[1]:.6f}.h5")
+
+# loading time: 31.9445sec
+# fitting time: 254.0593sec
+# LOSS: 0.396671
+# ACC:  0.832124
