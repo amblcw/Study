3ee2177a16bc030c2e5f327e214c48f4f3d3ede7
amblcw <amblcw@naver.com>
2024-03-05 15:16:21 +0900

    42

--- python/test.py
+++ python/test.py
@@ -5,9 +5,23 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 a = 10
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
 
+x = np.array([
+    [1,2,3,4,5],
+    [2,3,4,5,6],
+    [3,4,5,6,7],
+    ]).T
 
-#테스트 테스트
-'''
-a = (a>b) a : b
-'''
+y = np.array([
+    [10,20,30,40,50],
+    [20,30,40,50,60],
+]).T
+
+model = RandomForestRegressor()
+model.fit(x,y)
+r2 = model.score(x,y)
+pred = model.predict(x)
+print("r2: ",r2)
+print(pred)
